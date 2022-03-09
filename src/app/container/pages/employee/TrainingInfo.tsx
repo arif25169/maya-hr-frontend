2f664bef53d5232dda8dc5ee8ab68c6b52e4b176
LAPTOP-J3J95L6K\hp <merajhossain29@gmail.com>
2022-03-09 10:59:08 +0600

    --

--- src/app/container/pages/employee/TrainingInfo.tsx
+++ src/app/container/pages/employee/TrainingInfo.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react';
 import { useStoreActions, useStoreState } from '../../../store/hooks/easyPeasy';
-import { Button, Card, Col, message, Row, Steps, Form, Input, DatePicker, Select, InputNumber, Table, Space} from 'antd'
+import { Button, Card, Col, message, Row, Steps, Form, Input, DatePicker, Select, InputNumber, Table, Space, Tooltip, Divider, Popconfirm, Typography} from 'antd'
 import moment from 'moment';
-import { PlusOutlined, SaveOutlined } from '@ant-design/icons';
+import { DeleteOutlined, EditOutlined, PlusOutlined, SaveOutlined } from '@ant-design/icons';
 import { monitorEventLoopDelay } from 'perf_hooks';
+import Modal from 'antd/lib/modal/Modal';
 
 export default function TrainingInfo() {
 
+    const { Title } = Typography;
     const [form] = Form.useForm();
     const [updateForm] = Form.useForm();
     const [showTrainingFrom, setShowTrainingFrom] = useState<any>(false); 
@@ -15,7 +17,8 @@ export default function TrainingInfo() {
     const saveEmployeeTrainingInfo = useStoreActions((state) => state.hr.saveEmployeeTrainingInfo);
     const fetchEmployeeTrainingInfoList = useStoreActions((state) => state.hr.fetchEmployeeTrainingInfoList);
     const employeeTrainingInfoList = useStoreState((state) => state.hr.employeeTrainingInfoList);
-
+    const deleteEmployeeTrainingInfo = useStoreActions((state) => state.hr.deleteEmployeeTrainingInfo);
+    const [isModalVisible, setIsModalVisible] = useState<any>(false);
 
     useEffect(() => {
         fetchEmployeeTrainingInfoList(localStorage.getItem('employeeId'))
@@ -31,9 +34,9 @@ export default function TrainingInfo() {
             topicsCovered: value.topicsCovered,
             trainingTitle: value.trainingTitle,
             trainingYear:moment(value.trainingYear).year()
-          }
-          saveEmployeeTrainingInfo(postdata);
-          
+        }
+        saveEmployeeTrainingInfo(postdata);
+        onClickCancel();
     } 
 
     const trainingInfoFormShowHide = () => {
@@ -43,18 +46,103 @@ export default function TrainingInfo() {
 
     const onClickCancel = () => {
         setShowTrainingFrom(false);
+        setAddButtonDisable(false);
+    }
+
+    const deleteTrainingInfo = (value) => {
+        deleteEmployeeTrainingInfo(value);
+    }
+
+    const closeModalMethod = (e) => {
+        updateForm.resetFields();
+        setIsModalVisible(false);
+    }
+
+    const updateTrainingInfoSubmit = () => {
+
+    }
+
+    const editTrainingInfoMadal = (val) => {
+        setIsModalVisible(true);
+        
     }
 
     return (
         <>
             <Card title="Training Info">
+                <>
+                    {employeeTrainingInfoList ? (
+                        employeeTrainingInfoList.map((item, index) => (
+                            <>
+                                <Row>
+                                    <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 6}} lg={{ span: 6}} xl={{ span: 6}}>
+                                        <Title level={3}>Academic {(index + 1)}</Title>
+                                    </Col>
+                                    <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 6, offset:12}} lg={{ span: 6, offset:12}} xl={{ span: 6, offset:12}}>
+                                        <div style={{ float:"right" }}>
+                                            <Tooltip title="Edit">
+                                                <Button type='primary'  icon={<EditOutlined />} onClick={() => editTrainingInfoMadal(item?.trainingId)}/>
+                                            </Tooltip>
+                                            &nbsp;
+                                            <Popconfirm
+                                                title="Are you sure to delete this?"
+                                                okText="Yes"
+                                                cancelText="No"
+                                                onConfirm={() => deleteTrainingInfo(item?.trainingId)}
+                                            >
+                                            <Tooltip title="Delete">
+                                                <Button danger  icon={<DeleteOutlined />} />
+                                            </Tooltip>
+                                        </Popconfirm>
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <Divider style={{ marginTop: 0, }}/>
+                                <Row>
+                                    <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 6}} lg={{ span: 6}} xl={{ span: 6}}>
+                                        <Title level={5}>Training Title</Title>
+                                        <p>{item?.trainingTitle}</p>
+                                    </Col>
+                                    <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 6}} lg={{ span: 6}} xl={{ span: 6}}>
+                                        <Title level={5}>Country</Title>
+                                        <p>{item?.country}</p>
+                                    </Col>
+                                    <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 6}} lg={{ span: 6}} xl={{ span: 6}}>
+                                        <Title level={5}>Topics Covered</Title>
+                                        <p>{item?.topicsCovered}</p>
+                                    </Col>
+                                    <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 6}} lg={{ span: 6}} xl={{ span: 6}}>
+                                        <Title level={5}>Training Year</Title>
+                                        <p>{item?.trainingYear}</p>
+                                    </Col>
+                                    <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 6}} lg={{ span: 6}} xl={{ span: 6}}>
+                                        <Title level={5}>Institute</Title>
+                                        <p>{item.instituteName}</p>
+                                    </Col>
+                                    <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 6}} lg={{ span: 6}} xl={{ span: 6}}>
+                                        <Title level={5}>Duration</Title>
+                                        <p>{item?.duration}</p>
+                                    </Col>
+                                    <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 6}} lg={{ span: 6}} xl={{ span: 6}}>
+                                        <Title level={5}>Location</Title>
+                                        <p>{item?.location}</p>
+                                    </Col>
+                                </Row>
+                            </>
+                        ))
+                    ) : (
+                        ""
+                    )}       
+                </>
                 {showTrainingFrom == true ?
+                    <>
+                        <Divider />
                     <Form
-                            layout="vertical"
-                            id="educationInformation"
-                            form={form}
-                            onFinish={trainingInformationSubmit}
-                        >
+                        layout="vertical"
+                        id="educationInformation"
+                        form={form}
+                        onFinish={trainingInformationSubmit}
+                    >
                         <Row>
                             <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 6}} lg={{ span: 6}} xl={{ span: 6}}>
                                 <Form.Item
@@ -155,10 +243,117 @@ export default function TrainingInfo() {
                             
                         </Row>
                     </Form>
+                    </>
                 : ""}
                 <Button type="primary"  icon={<PlusOutlined />} onClick={() => trainingInfoFormShowHide()} disabled={addButtonDisable == true ? true : false}>
-                    Add Education (If Required)
+                    Add Training (If Required)
                 </Button>
+                <Modal
+                    title="Update Training Information"
+                    visible={isModalVisible}
+                    //  onOk={handleOk}
+                    okButtonProps={{ form: 'update', htmlType: 'submit' }}
+                    onCancel={(e) => closeModalMethod(e)}
+                    cancelText="Close"
+                    okText="Update"
+                    centered
+                    maskClosable={false}
+                    width={1000}
+                >
+                    <Form
+                        layout="vertical"
+                        id="update"
+                        onFinish={updateTrainingInfoSubmit}
+                        form={updateForm}
+                    >
+                        <Row>
+                            <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 8}} lg={{ span: 8}} xl={{ span: 8}}>
+                                <Form.Item
+                                    name="trainingTitleUpdate"
+                                    label="Training Title"
+                                    className="title-Text"
+                                    rules={[
+                                        { required: true, message: "Please Write Training Title" },
+                                    ]}
+                                >
+                                    <Input placeholder="Write Training Title" />
+                                </Form.Item>
+                            </Col>
+                            <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 8}} lg={{ span: 8}} xl={{ span: 8}}>
+                                <Form.Item
+                                    name="countryNameUpdate"
+                                    label="Country Name"
+                                    className="title-Text"
+                                    rules={[
+                                        { required: true, message: "Please Write Country Name" },
+                                    ]}
+                                >
+                                    <Input placeholder="Write Country Name" />
+                                </Form.Item>
+                            </Col>
+                            <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 8}} lg={{ span: 8}} xl={{ span: 8}}>
+                                <Form.Item
+                                    name="topicsCoveredUpdate"
+                                    label="Topics Covered"
+                                    className="title-Text"
+                                    rules={[
+                                        { required: true, message: "Please Write Topics Covered" },
+                                    ]}
+                                >
+                                    <Input placeholder="Write Topics Covered" />
+                                </Form.Item>
+                            </Col>
+                            <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 8}} lg={{ span: 8}} xl={{ span: 8}}>
+                                <Form.Item
+                                    name="trainingYearUpdate"
+                                    label="Training Year"
+                                    className="title-Text"
+                                    rules={[
+                                        { required: true, message: "Please Select Training Year" },
+                                    ]}
+                                >
+                                    <DatePicker picker="year" style={{ width: "100%" }}/>
+                                </Form.Item>
+                            </Col>
+                            <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 8}} lg={{ span: 8}} xl={{ span: 8}}>
+                                <Form.Item
+                                    name="instituteUpdate"
+                                    label="Institute"
+                                    className="title-Text"
+                                    rules={[
+                                        { required: true, message: "Please Write Institute" },
+                                    ]}
+                                >
+                                    <Input placeholder="Write Instiute" />
+                                </Form.Item>
+                            </Col>
+                            <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 8}} lg={{ span: 8}} xl={{ span: 8}}>
+                                <Form.Item
+                                    name="durationUpdate"
+                                    label="Duration"
+                                    className="title-Text"
+                                    rules={[
+                                        { required: true, message: "Please Write Duration" },
+                                    ]}
+                                >
+                                    <InputNumber placeholder="Write Duration" />
+                                </Form.Item>
+                            </Col>
+                            <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 8}} lg={{ span: 8}} xl={{ span: 8}}>
+                                <Form.Item
+                                    name="locationUpdate"
+                                    label="location"
+                                    className="title-Text"
+                                    rules={[
+                                        { required: true, message: "Please Write Location" },
+                                    ]}
+                                >
+                                    <Input placeholder="Write Location" />
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                    </Form>
+                </Modal>
             </Card>
         </>
     )
